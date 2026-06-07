b55fdd1ba18f620bde3561b865ebaf24d8a646b7
hendo.hpp <270048391+hendo-hpp@users.noreply.github.com>
2026-06-07 15:31:20 -0400

    feat(network): add TCP listener to handle client connections (#1)

---
+++ server/include/config/network.hpp
@@ -0,0 +1,16 @@
+#ifndef SYNCOPATE_CONFIG_NETWORK_HPP__
+#define SYNCOPATE_CONFIG_NETWORK_HPP__
+
+#include <cstdint>
+#include <string_view>
+
+namespace syncopate::config::network {
+
+constexpr uint16_t DEFAULT_PORT = 8080;
+constexpr int BACKLOG_COUNT = 20;
+
+// todo: api endpoints
+
+} // namespace syncopate::config::network
+
+#endif // SYNCOPATE_CONFIG_NETWORK_HPP__
